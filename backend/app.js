f75200e0b98aa5c5e54ac3f65ee681941c6881e6
magisystem0408 <masato.matsudo@gmail.com>
2022-03-22 09:46:42 +0900

    add dotenv

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {graphqlHTTP} = require('express-graphql')
 const mongoose = require('mongoose')
 const schema = require('./schema/schema')
+const dotenv =require("dotenv")
 const app = express()
+dotenv.config({path:__dirname+"/.env"})
 
 
 //catchをやっていないためエラー出る。
-mongoose.connect('mongodb+srv://admin:Mamushi3timi@cluster0.1pvuc.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
